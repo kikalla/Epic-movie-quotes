98b2a0ae1336ea9aa9645faebf79c3d3cddedd02
kikadzeRedberry <giorgikikadze@redberry.ge>
2022-11-19 15:50:41 +0400

    style: made landing page style

---
+++ epic-movie-quotes-vue/src/components/layout/PageHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+    class="bg-black w-full h-24 flex px-[4.5rem] items-center justify-between fixed top-0 z-50"
+  >
+    <h2 class="text-white text-base font-medium">MOVIE QUOTES</h2>
+    <div>
+      <select name="lang" id="lang" class="h-7 text-white bg-black">
+        <option value="en">Eng</option>
+        <option value="ka">Geo</option>
+      </select>
+      <button
+        class="text-white text-base h-9 bg-[#E31221] rounded-lg w-28 mx-6 font-normal hover:bg-[#CC0E10]"
+      >
+        Sign Up
+      </button>
+      <button
+        class="text-white text-base border-white border h-9 rounded-lg w-28 font-normal"
+      >
+        Log In
+      </button>
+    </div>
+  </div>
+</template>
