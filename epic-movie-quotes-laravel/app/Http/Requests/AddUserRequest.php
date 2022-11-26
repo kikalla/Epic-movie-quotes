ae4a091cce3b7dffa3f4df7cd50af3a8e482ef1b
kikadzeRedberry <giorgikikadze@redberry.ge>
2022-11-26 22:11:43 +0400

    fix: add regex validation on username laravel side

--- epic-movie-quotes-laravel/app/Http/Requests/AddUserRequest.php
+++ epic-movie-quotes-laravel/app/Http/Requests/AddUserRequest.php
@@ -14,7 +14,7 @@ class AddUserRequest extends FormRequest
 	public function rules()
 	{
 		return [
-			'username'                  => 'required|min:3|max:15|unique:users,username',
+			'username'                  => 'required|min:3|max:15|unique:users,username|regex:/^[a-z0-9!@#\$%\^\&*\)\(+=._-]+$/',
 			'email'                     => 'required|unique:users,email|email',
 			'password'                  => 'min:8|max:15|required',
 			'password_confirmation'     => 'same:password, required',
